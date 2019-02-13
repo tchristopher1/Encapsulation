412648c40ca14d780356d55623a817ce8f9c4b70
VikonGit <46765750+VikonGit@users.noreply.github.com>
2019-02-13 17:03:26 -0500

    Update TestEncapsulation.java

--- TestEncapsulation.java
+++ TestEncapsulation.java
@@ -8,7 +8,7 @@ public class TestEncapsulation
         Encapsulate obj = new Encapsulate(); 
     	
     	
-    	System.out.println("Please enter a password, 10-15 characters, use numbers and letters must contain at least one Capital.");
+    	System.out.println("Please enter a password, 10-15 characters, use numbers and letters, must contain at least one Capital.");
 
     	//Use setPassword to write to value
         obj.setPassword(in.next()); 
